ad8a839381ceee2733a5d47fc422a453328f6f92
GuilhermeSilva <guilherme.silva@klab.com.br>
2024-08-28 21:54:56 -0300

    feature(HubScreen): tela principal de hub de opções de apis

---
+++ app/src/main/java/br/com/fiap/hubappworld/screens/hub/HubScreen.kt
@@ -0,0 +1,37 @@
+package br.com.fiap.hubappworld.screens.hub
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.hubappworld.R
+import br.com.fiap.hubappworld.components.CardHub
+
+@Composable
+fun HubScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD4DCFF)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(verticalArrangement = Arrangement.spacedBy(-35.dp)) {
+            CardHub(painterResource(id = R.drawable.clima), "Icone do clima")
+            CardHub(painterResource(id = R.drawable.monetization_on_24), "Icone de dinheiro")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun HubPreview() {
+    HubScreen()
+}
