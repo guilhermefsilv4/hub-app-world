a514d91679e34ee0c51ed7d9dd5a32772ae2321c
GuilhermeSilva <guilherme.silva@klab.com.br>
2024-08-28 21:55:26 -0300

    feature(MainActivity): chamada do hub como tela principal

--- app/src/main/java/br/com/fiap/hubappworld/MainActivity.kt
+++ app/src/main/java/br/com/fiap/hubappworld/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import br.com.fiap.hubappworld.screens.HubScreen
+import br.com.fiap.hubappworld.screens.hub.HubScreen
 import br.com.fiap.hubappworld.ui.theme.HubAppWorldTheme
 
 class MainActivity : ComponentActivity() {
